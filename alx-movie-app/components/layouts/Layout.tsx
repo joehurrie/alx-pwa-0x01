76b7acbad4d02921ffc3267d4f8e22536879defa
dricko147 <fred.eseka@gmail.com>
2025-02-08 08:50:40 -0800

    link manifest to application for PWA functionality

--- alx-movie-app/components/layouts/Layout.tsx
+++ alx-movie-app/components/layouts/Layout.tsx
@@ -3,13 +3,13 @@ import Header from "./Header";
 import Footer from "./Footer";
 
 const Layout: React.FC<ComponentProps> = ({ children }) => {
-  return (
-    <>
-      <Header />
-      <main className="min-h-screen">{children}</main>
-      <Footer />
-    </>
-  )
-}
+    return (
+        <>
+            <Header />
+            <main className="min-h-screen">{children}</main>
+            <Footer />
+        </>
+    );
+};
 
 export default Layout;
